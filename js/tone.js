fe406f75e3bf1b718cf34c124b6d90fed804e207
Tristan Trim <TristanTrim@gmail.com>
2017-01-02 16:49:39 -0400

    Lines from the scale drawn!

--- js/tone.js
+++ js/tone.js
@@ -1,5 +1,4 @@
 
-
 // Audio stuff
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var is_tone = false;
@@ -31,22 +30,43 @@ function stop_tone(){
 
 var container = document.querySelector("#contentContainer");
 container.addEventListener("click", getClickPosition, false);
+//var myGamePiece = new component(30, 30, "red", 10, 20);
 
-// stuff for calculating pitch
 var container_value_start = 0;
 var container_value_end = 500;
 var scale_start = 131;
 var scale_end = 523;
 var scale_buf = 5;
 var scale_duration = scale_end - scale_start + 2*scale_buf; 
+function frequency2position(frequency){
+  return ((frequency-scale_start)/scale_duration)*container_value_end;
+}
+function position2frequency(position){
+  return (position/container_value_end)*scale_duration
+                +scale_start;//-scale_buf;
+
+}
+// drawing a line
+var ctx;
+var frequency;
+for (var note in scale) {
+  frequency = scale[note];
+  if (scale_start <= frequency && frequency <= scale_end){
+    var position = frequency2position(frequency);
+    ctx=container.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(position,0);
+    ctx.lineTo(position,30);
+    ctx.stroke();
+  }
+}
 
 // The clicking on the canvas event
 function getClickPosition(e) {
     var parentPosition = getPosition(e.currentTarget);
     var xPosition = e.clientX - parentPosition.x;// - (theThing.clientWidth / 2);
     var yPosition = e.clientY - parentPosition.y;// - (theThing.clientHeight / 2);
-    var tone = (xPosition/container_value_end)*scale_duration
-                +scale_start;//-scale_buf;
+    var tone = position2frequency(xPosition);
     if (is_tone){
       stop_tone();
     }
