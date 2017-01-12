7c37537efd7e344d8a7af710f4dd229b923eec15
TristanTrim <TristanTrim@gmail.com>
2017-01-12 13:05:52 -0400

    Some cleaning up.

--- js/tone.js
+++ js/tone.js
@@ -3,16 +3,12 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var is_tone = false;
 var oscillator;
-
 function tone_init(pitch){
-// create Oscillator node
-oscillator = audioCtx.createOscillator();
-
-oscillator.type = 'sine';
-oscillator.frequency.value = pitch; // value in hertz
-oscillator.connect(audioCtx.destination);
+  oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.value = pitch; // value in hertz
+  oscillator.connect(audioCtx.destination);
 }
-
 function play_tone(pitch){
   if (! is_tone) {
     tone_init(pitch);
@@ -25,12 +21,9 @@ function stop_tone(){
   is_tone = false;
 }
 
-
 // The game canvas
-
 var container = document.querySelector("#contentContainer");
 container.addEventListener("click", getClickPosition, false);
-//var myGamePiece = new component(30, 30, "red", 10, 20);
 
 var ctx=container.getContext("2d");
 var input_width = 490;//Why is this a thing? Ack!
@@ -59,27 +52,13 @@ for (var note in scale) {
     ctx.lineTo(position,30);
     ctx.stroke();
     //if (note.startsWith("C")&& !note.endsWith("#")) {
-      ctx.font = "12px serif";
-      ctx.fillText(note, position, 42+20*(note_vertical_offset_count%3));
+      ctx.font = 12 + "px serif";
+      ctx.fillText(note, position, 42+15*(note_vertical_offset_count%5));
     //}
   }
   note_vertical_offset_count++;
 }
 
-// The clicking on the canvas event
-function getClickPosition(e) {
-    var parentPosition = getPosition(e.currentTarget);
-    var xPosition = e.clientX - parentPosition.x;// - (theThing.clientWidth / 2);
-    var yPosition = e.clientY - parentPosition.y;// - (theThing.clientHeight / 2);
-    var tone = position2frequency(xPosition);
-    console.log(xPosition);
-    console.log(tone);
-    if (is_tone){
-      stop_tone();
-    }
-    play_tone(tone); 
-    //alert(tone);
-}
  
 // Helper function to get an element's exact position
 function getPosition(el) {
@@ -107,3 +86,21 @@ function getPosition(el) {
     y: yPos
   };
 }
+
+
+// The clicking on the canvas event
+function getClickPosition(e) {
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x;// - (theThing.clientWidth / 2);
+    var yPosition = e.clientY - parentPosition.y;// - (theThing.clientHeight / 2);
+    var tone = position2frequency(xPosition);
+    console.log(xPosition);
+    console.log(tone);
+    if (is_tone){
+      stop_tone();
+    }
+    play_tone(tone); 
+    //alert(tone);
+}
+
+
